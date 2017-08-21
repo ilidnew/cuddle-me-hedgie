e95817755941985cf4f98d40d06b1e956e1ccac6
Wendi Li <wendicodes@gmail.com>
2017-08-21 18:58:20 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :hedgies, only: [:new, :create, :show, :search]
+  resources :hedgies, only: [:create, :search]
+  resources :hedgies, only: [:new, :show], as: :hedgie
 end
