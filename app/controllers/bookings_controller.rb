8d824f3a379ead7867f9dfd60b69740d3424ef86
Wendi Li <wendicodes@gmail.com>
2017-08-23 13:09:04 +0200

    pull from master

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   def index
-
+    @user = current_user
+    @bookings = Booking.where(user_id: current_user.id)
   end
 
   def new
@@ -10,15 +11,22 @@ class BookingsController < ApplicationController
 
   def create
     @hedgie = Hedgie.find(params[:hedgy_id])
-    @booking = Booking.new(booking_params)
+    start_date = Date.strptime(params[:booking][:start_date], '%m/%d/%Y')
+    end_date = Date.strptime(params[:booking][:end_date], '%m/%d/%Y')
+    @booking = Booking.new(start_date: start_date, end_date: end_date)
     @booking.user = current_user
     @booking.hedgie = @hedgie
 
+
     if @booking.save
-      redirect_to profile_path(@booking.user)
+      redirect_to bookings_path
     else
+      Rails.logger.info(@booking.errors.messages)
       render :new
     end
+  end
+
+  def show
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,19 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+
   resources :profiles, only: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :hedgies do
-
-    resources :bookings, only: [:new, :create, :index] do
-      resources :reviews, only: [:new, :create, :destroy]
-    end
-
+    resources :bookings, only: [:new, :create]
   end
+
+  resources :bookings, only: [:show, :index] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
   mount Attachinary::Engine => "/attachinary"
 end
