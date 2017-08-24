95a6e67f33555d0039074e80e1f32f663bd4e865
Caspar Wikstrom <caspar.wikstrom@gmail.com>
2017-08-24 14:01:34 +0200

    done with redirecting after login and logout

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: true
+  # validates :rating, inclusion: { in: [1,2,3,4,5]}
+  validates :rating, inclusion: { in: [1,2,3,4,5]}
   validates :content, presence: true
-
 end
