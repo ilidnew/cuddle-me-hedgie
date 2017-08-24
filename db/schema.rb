f81796c4d6318eb1da96a46220c4b4243ab08b51
Caspar Wikstrom <caspar.wikstrom@gmail.com>
2017-08-24 16:34:51 +0200

    added statust to bookings and set default to pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823182727) do
+ActiveRecord::Schema.define(version: 20170824141626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(version: 20170823182727) do
   create_table "bookings", force: :cascade do |t|
     t.date     "start_date"
     t.date     "end_date"
-    t.string   "status"
+    t.string   "status",     default: "pending"
     t.integer  "user_id"
     t.integer  "hedgie_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.index ["hedgie_id"], name: "index_bookings_on_hedgie_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
